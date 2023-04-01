7eccb7a2a9c45c59debf2cc02ab0f18a30598cac
Macil Tamazirt <maciltam@gmail.com>
2023-04-02 00:00:53 +0100

    wifi

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ String toDigits(int num);
 void publish_num_balls(WebSocketsClient &webSocket);
 void publish_turtle_vel(WebSocketsClient &webSocket);
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
-///////////////////////////////////
+
 // Global vars
 volatile int num_balls = 0;
 volatile int score = 0;
 
 // Network settings
-const char *ssid = "MT";
-const char *pass = "EL604QN8GR8";
-const char *serverIP = "192.168.8.100";
+const char *ssid = "ssid";
+const char *pass = "pass";
+const char *serverIP = "192.168.0.0";
 
 // Display
 const int rs = 32, en = 33, d4 = 26, d5 = 27, d6 = 14, d7 = 12;
@@ -67,8 +67,8 @@ void setup()
   delay(1000);
   lcd.clear();
   ////////Setup communication with rosbridge package
+  //webSocket.sendTXT("{\"op\": \"advertise\", \"topic\": \"/turtle1/cmd_vel\", \"type\": \"geometry_msgs/msg/Twist\"}");
   webSocket.sendTXT("{\"op\": \"advertise\", \"topic\": \"/basket/num_balls\", \"type\": \"std_msgs/Int32\"}");
-  webSocket.sendTXT("{\"op\": \"advertise\", \"topic\": \"/turtle1/cmd_vel\", \"type\": \"geometry_msgs/msg/Twist\"}");
   webSocket.sendTXT("{\"op\":\"subscribe\",\"topic\":\"/basket/score\",\"type\":\"std_msgs/msg/Int32\"}");
 
   lcd.print("finished setup");
